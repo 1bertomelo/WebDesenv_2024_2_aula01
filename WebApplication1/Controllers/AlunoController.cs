742b7aea0fc39476d4a497e36709c0ff7ee770bb
betomelo_9 <betomelo_9@hotmail.com>
2024-08-14 19:08:37 -0300

    Add project files.

---
+++ WebApplication1/Controllers/AlunoController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	[ApiController]
+	[Route("api/aluno")]
+	public class AlunoController : ControllerBase
+	{
+		private static List<DadosAluno> dadosAlunosList =  new List<DadosAluno>();
+
+		public AlunoController()
+		{
+
+		}
+
+		[HttpGet]
+		public IActionResult OlaAluno()
+		{
+			return Ok("Ola Mundo");
+		}
+
+		[HttpGet]
+		[Route("ObterTodos")]
+		public IActionResult ObterTodos()
+		{
+			return Ok(dadosAlunosList);
+		}
+
+		[HttpPost]
+		public IActionResult OlaAlunoNome(string nome)
+		{
+			return Ok($"Ola Mundo {nome}");
+		}
+
+		[HttpPost]
+		[Route("Inserir")]
+		public IActionResult Inserir(DadosAluno dados)
+		{
+			dadosAlunosList.Add(dados);
+			return Ok($"Aluno(a) {dados.nome} inserido com sucesso.");
+		}
+	}
+}
---
+++ WebApplication1/Models/DadosAluno.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+	public class DadosAluno
+	{
+		public string nome { get; set; }
+		public string email { get; set; }
+		public string telefone { get; set; }
+	}
+}
